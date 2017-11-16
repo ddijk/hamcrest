6229956996d3827c92ff86b07fea1dd58f432b1a
Dick Dijk <ddijk@bkwi.nl>
2017-11-16 16:14:04 +0100

    Add tests for list

--- src/test/java/snippets/dijkrosoft/nl/HamcrestTest.java
+++ src/test/java/snippets/dijkrosoft/nl/HamcrestTest.java
@@ -1,10 +1,17 @@
 package snippets.dijkrosoft.nl;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.everyItem;
+import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.not;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.Test;
@@ -20,6 +27,21 @@ public class HamcrestTest {
 
         assertThat(persons, hasKey("Jens") );
         assertThat(persons, not(hasKey("Dick")) );
+
+    }
+
+    @Test
+    public void testList() {
+        List<Integer> list = Arrays.asList(5, 2, 4);
+
+        assertThat(list, hasSize(3));
+
+        // ensure the order is correct
+        assertThat(list, contains(5, 2, 4));
+
+        assertThat(list, containsInAnyOrder(2, 4, 5));
+
+        assertThat(list, everyItem(greaterThan(1)));
 
     }
 }
